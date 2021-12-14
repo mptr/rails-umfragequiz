564c94e2cdd5f576daee9b36fbe978059eef0014
mptr <54647612+mptr@users.noreply.github.com>
2021-12-14 18:28:57 +0100

    validations email & date

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,10 +5,10 @@ class Survey < ApplicationRecord
 
 	# validation
 	validates :name, presence: true, uniqueness: true
-	validate :datetime_valid_from
-	validate :datetime_valid_to
-
-	# eigene Validierungsmethode
-	# TODO: fromDate muss vor toDate liegen
-
+	validate :from_date_before_to_date
+	def from_date_before_to_date
+		if from_date.present? && to_date.present? && from_date > to_date
+			errors.add(:from_date, "must be before to_date")
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
     has_many :submission_sets
 
     # validation
-    validates :username, presence: true, uniqueness: true, format: { with: /^\w{3,}$/, message: ' - mindestens 3 nicht-leere Zeichen!' }
-    validates :email, presence: true, uniqueness: true, format: { with: /^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$/, message: ' - gültige E-Mail-Adresse verwenden!' }
+    validates :username, presence: true, uniqueness: true, format: { with: /\A\w{3,}\z/, message: ' - mindestens 3 nicht-leere Zeichen!' }
+    validates :email, presence: true, uniqueness: true, format: {
+        with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i,
+        message: ' - gültige E-Mail-Adresse verwenden!'
+    }
 end
