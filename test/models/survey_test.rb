2c733880b497d4280b314346963f4ff9316f2660
mptr <54647612+mptr@users.noreply.github.com>
2021-12-20 15:49:05 +0100

    remove generated fixture contents + erste tests

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class SurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Survey validations" do
+    s = Survey.new
+    assert_not s.save, "Saved survey without name"
+    s.name = ""
+    assert_not s.save, "Saved survey with empty name"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User validations" do
+    u = User.new
+    assert_not u.save, "Saved user without email & username"
+    u.email = "someemail"
+    assert_not u.save, "Saved user without username"
+    u.username = "Testuser01"
+    assert_not u.save, "Saved user with invalid email"
+    u.email = "testuser@mydomain.com"
+    assert u.save, "Failed to save user"
+  end
 end
