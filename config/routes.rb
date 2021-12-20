5be456415de6e85ff53e860e6ea81952043a12c1
bemme001 <emmibe@gmx.de>
2021-12-20 19:23:16 +0100

    routes angepasst

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,12 @@
 Rails.application.routes.draw do
-  # TODO: configure route structure like:
-    # User
-    #  ┕ Survey
-    # Survey
-    #  ┕ Question
-    #  ┕ SubmissionSet
-    #    ┕ Submission
-  # (no routes for specialized types like NPSQuestion, ColorQuestion, StringSubmission, ...)
-  resources :single_choice_questions
-  resources :multiple_choiche_questions
-  resources :likert_questions
-  resources :prio_questions
-  resources :multiple_number_submissions
-  resources :rearrangeable_questions
-  resources :string_questions
-  resources :slider_questions
-  resources :number_questions
-  resources :number_range_questions
-  resources :nps_questions
-  resources :number_submissions
-  resources :color_questions
-  resources :text_submissions
-  resources :text_questions
-  resources :submissions
-  resources :questions
-  resources :submission_sets
-  resources :surveys
-  resources :users
+  resources :users do
+    resources :surveys
+  end
+  resources :surveys do
+    resources :questions
+    resources :submission_sets do
+      resources :submissions
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
