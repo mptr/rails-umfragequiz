b08af36aa9393ef54912675e06bc438546346088
mptr <54647612+mptr@users.noreply.github.com>
2022-01-10 15:09:51 +0100

    check for bearer token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :check_auth
+
+  def check_auth
+
+
+    puts request.headers['HTTP_AUTHORIZATION']
+    render :nothing => true, :status => 403 unless request.headers['HTTP_AUTHORIZATION']
+  end
 end
