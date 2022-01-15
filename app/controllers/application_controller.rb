c4fa126bf6598c5f2e4027dc575ca699ae8cbeab
mptr <54647612+mptr@users.noreply.github.com>
2022-01-15 14:02:02 +0100

    validation todo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::API
   before_action :check_auth
 
   def check_auth
-
-
+    # decode middle jwt part
+    # if iss == 'google'
+      # validation with google
+    # else if iss == 'microsoft'
+      # validation with microsoft
+    # else
+      # reject w 403
     puts request.headers['HTTP_AUTHORIZATION']
     render :nothing => true, :status => 403 unless request.headers['HTTP_AUTHORIZATION']
   end
