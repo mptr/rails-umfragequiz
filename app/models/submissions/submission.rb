d4b9c0053474ffa5ed16866e5fcdd8a84d5bc999
bemme001 <emmibe@gmx.de>
2022-01-22 15:49:14 +0100

    + type of submission

--- app/models/submissions/submission.rb
+++ app/models/submissions/submission.rb
@@ -2,6 +2,12 @@ class Submission < ApplicationRecord
     belongs_to :submission_set
     belongs_to :question
 
+    def as_json options={}
+        super.as_json(options).merge({type:type})
+    end
+
+    attribute :type
+
     # validation
     validates :answer, presence: true
 end
